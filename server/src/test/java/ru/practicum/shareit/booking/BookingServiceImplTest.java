b6813c57cae605d8e8fb81b9ee2407e219bb1bf6
Denis Orlov <des2033@gmail.com>
2026-07-15 21:30:54 +0400

    12

--- server/src/test/java/ru/practicum/shareit/booking/BookingServiceImplTest.java
+++ server/src/test/java/ru/practicum/shareit/booking/BookingServiceImplTest.java
@@ -15,13 +15,11 @@ import ru.practicum.shareit.user.User;
 import ru.practicum.shareit.user.repository.UserRepository;
 
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
